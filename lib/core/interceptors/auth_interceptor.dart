3b52ab481eb93b7c9c5ea63ab8b5a28a7a957537
Elissandro Silva Soares <elissandro.soares@wakke.co>
2022-12-02 15:16:22 -0300

    added temporary refresh token exemple

--- lib/core/interceptors/auth_interceptor.dart
+++ lib/core/interceptors/auth_interceptor.dart
@@ -1,11 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:wakke_manager_v2/core/constants/app_colors.dart';
+import 'package:wakke_manager_v2/core/interfaces/local_storage_interface.dart';
 
 class AuthInterceptor extends InterceptorsWrapper {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers.addAll({"Authorization": "Bearer token aqui"});
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    // TODO: PASSAR PARA UM OBSERVABLE
+    final localStorage = Modular.get<ILocalStorage>();
+    String token = await localStorage.getString('token');
+
+    options.headers.addAll({"Authorization": "bearer $token"});
     super.onRequest(options, handler);
   }
 
